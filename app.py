34b5f3d1ac4c76ab267491fed0d55bd558e41ae0
gseganzerla <g.seganzerla@gmail.com>
2024-02-01 20:54:06 -0300

    logger func

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 import requests
 
 app = Flask(__name__)
+CORS(app)
 
 
-@app.route("/classification")
+
+@app.route("/classification", methods=['POST'])
 def classification():
     response = requests.get('http://127.0.0.1:5001').json()
+ 
     return response
 
 @app.route("/regression")
