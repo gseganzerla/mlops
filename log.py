a8edd7ddb9f477213404e8f89a3688e7fe060af8
gseganzerla <g.seganzerla@gmail.com>
2024-02-04 20:25:42 -0300

    ajustando funcao de log

--- log.py
+++ log.py
@@ -8,8 +8,3 @@ def write_log(request_data, response_data):
         log = f"""request_data: {request_data}, response_data: {response_data}, date: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n"""
 
         file.write(log)
-
-
-write_log({'gender': 'Female', 'Year': 2024}, {'defaulter': True})
-write_log({'gender': 'Male', 'Year': 2023}, {'defaulter': False})
-write_log({'gender': 'Female', 'Year': 2022}, {'defaulter': True})
