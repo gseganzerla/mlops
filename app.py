6f394bb6fced456d9108c841a8449a93a207e440
gseganzerla <g.seganzerla@gmail.com>
2024-02-04 11:47:37 -0300

    final

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import requests
-
+from log import write_log
 app = Flask(__name__)
 CORS(app)
 
@@ -14,14 +14,19 @@ def classification():
     data = request.get_json()
     response = requests.post('http://classification:5001', json=data, headers=default_headers)
 
-    return response.json()
+    result = response.json()
+
+    write_log(data, result)
+    return result
 
 @app.route("/clusterization", methods=['POST'])
-def regression():
+def clusterization():
     data = request.get_json()
     response = requests.post('http://clusterization:5002', json=data, headers=default_headers)
-    return response.json()
+    result = response.json()
 
+    write_log(data, result)
+    return result
 
 if __name__ == '__main__':
     app.run('0.0.0.0', 5000)
